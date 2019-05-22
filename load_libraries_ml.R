74f8af418e518cd901779cee6d048a6f5f43579b
Paul Jacques-Mignault <44065589+paul-jm@users.noreply.github.com>
2019-05-22 22:55:30 +0200

    Add files via upload

---
+++ load_libraries_ml.R
@@ -0,0 +1,42 @@
+packages_list <- c('data.table',
+                   'lubridate',
+                   'ggplot2',
+                   'ggiraph',
+                   'ggthemes',
+                   'tidyverse',
+                   'caret',
+                   'rpart.plot',
+                   'partykit', 
+                   'randomForest',
+                   'xgboost',
+                   'MASS',
+                   'visNetwork',
+                   'sparkline',
+                   'glmnet',
+                   'scales',
+                   'gridExtra',
+                   'leaflet',
+                   'corrplot',
+                   'psych',
+                   'MLmetrics',
+                   'class',
+                   'rpart',
+                   'ranger',
+                   'dplyr',
+                   'mlbench',
+                   'Hmisc',
+                   'grid',
+                   'reshape2'
+)
+
+
+for (i in packages_list){
+  if(!i%in%installed.packages()){
+    install.packages(i, dependencies = TRUE, repos = "http://cran.us.r-project.org")
+    library(i, character.only = TRUE)
+    print(paste0(i, ' has been installed'))
+  } else {
+    print(paste0(i, ' is already installed'))
+    library(i, character.only = TRUE)
+  }
+}
