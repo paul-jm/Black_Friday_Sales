2ae5409892261e27b9ededa07a345db13c6b95ce
Paul Jacques-Mignault <44065589+paul-jm@users.noreply.github.com>
2019-05-22 22:24:59 +0200

    Add files via upload

---
+++ f_partition_sirio.R
@@ -0,0 +1,10 @@
+f_partition<-function(df,test_proportion=0.2, seed=NULL){
+  
+  if(!is.null(seed)) set.seed(seed) # Tool to specify seed, important
+  
+  train_index<-sample(nrow(df), floor(nrow(df)*(1-test_proportion)), replace = FALSE)
+  df_train<-df[train_index,]
+  df_test<-df[-train_index,]
+  
+  return(list(train=df_train, test=df_test))
+}
